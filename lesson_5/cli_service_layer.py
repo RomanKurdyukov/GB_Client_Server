82ca05ae203e6a4ca26fde43448c59d06b9d1431
Roman <afonasev@gmail.com>
2022-01-10 23:30:28 +0300

    Hometask 5 has been completed...

---
+++ lesson_5/cli_service_layer.py
@@ -0,0 +1,35 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import yaml
+from time import time
+import base64
+
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+
+with open('app_config.yaml', 'r') as config:
+    client_config = yaml.load(config, Loader=yaml.Loader)
+
+
+def encode_data(data):
+    encoded_answer = str(data).encode(client_config['ENCODING'])
+    encoded_data = base64.b64encode(encoded_answer)
+    return encoded_data
+
+
+def decode_data(data):
+    decoded_data = eval(base64.b64decode(data))
+    return decoded_data
+
+
+def presence_msg_create(username):
+    presence_data = {
+        "action": client_config['ACTION'][3],
+        "time": time(),
+        "type": "status",
+        "user": {
+            "account_name": username,
+            "status": "Yep, I am here!"
+        }
+    }
+    return presence_data
+
